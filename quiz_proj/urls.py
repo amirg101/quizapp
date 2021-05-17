cff28410e801feba3fec55032b4988d278a6e2c2
amirg101 <amirgazi101@gmail.com>
2021-05-17 20:30:17 +0530

    second2

--- quiz_proj/urls.py
+++ quiz_proj/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('quizes.urls',namespace='quizes'))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
